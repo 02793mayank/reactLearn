8414e5c4891f24db9a93d3de0ac0ce2a6d933c0a
02793mayank <mayanksahu02793@gamail.com>
2025-07-30 23:38:16 +0530

    new things

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Myfunction from './myfunction.jsx'
 
+function MyApp(){
+  return 
+  <h2>this direct app</h2>
+}
+const ReactELement={
+    type:'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    Children: 'click me to visit google'
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <App/>
+     <App/>
       <Myfunction/>
-      <h4>this is we can only render one tag so we sum up all the tags in one empty tag</h4>
+      <MyApp/>
+      <h4>we can only render one tag so we sum up all the tags in one empty tag</h4>
+      ReactELement
     </>
   </StrictMode>,
 )
